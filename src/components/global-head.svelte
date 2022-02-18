acb286639078a1755ce9f10304c79399ba5779d9
Quentin Bellanger <hello@quentin-bellanger.com>
2022-02-18 22:24:45 +0100

    use  instead of window to get url

--- src/components/global-head.svelte
+++ src/components/global-head.svelte
@@ -1,12 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 
 	let url;
 	let ogImageUrl;
 
 	onMount(() => {
-		url = window.location.href;
-		ogImageUrl = `${window.location.origin}/og.png`;
+		url = `${$page.host}${$page.path}`;
+		ogImageUrl = `${$page.host}/og.png`;
 	});
 </script>
 
@@ -14,9 +15,9 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="icon" href="/favicon.png" />
-	<meta property="og:url" name="og:url" content={url} />
-	<meta property="og:image" name="og:image" content={ogImageUrl} />
-	<meta property="og:type" name="og:type" content="website" />
+	<meta property="og:url" content={url} />
+	<meta property="og:image" content={ogImageUrl} />
+	<meta property="og:type" content="website" />
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:image" content={ogImageUrl} />
