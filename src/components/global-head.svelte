8690275ec54d065f10e89f4f0d5552d671a269a8
Quentin Bellanger <hello@quentin-bellanger.com>
2022-02-18 22:20:04 +0100

    add name attribute to og tags

--- src/components/global-head.svelte
+++ src/components/global-head.svelte
@@ -14,9 +14,9 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="icon" href="/favicon.png" />
-	<meta property="og:url" content={url} />
-	<meta property="og:image" content={ogImageUrl} />
-	<meta property="og:type" content="website" />
+	<meta property="og:url" name="og:url" content={url} />
+	<meta property="og:image" name="og:image" content={ogImageUrl} />
+	<meta property="og:type" name="og:type" content="website" />
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:image" content={ogImageUrl} />
