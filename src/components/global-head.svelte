e396edcf0bc68355962b21684b931cac6cc78976
Quentin Bellanger <hello@quentin-bellanger.com>
2022-02-18 22:10:14 +0100

    move og and favicon to root folder

--- src/components/global-head.svelte
+++ src/components/global-head.svelte
@@ -6,14 +6,14 @@
 
 	onMount(() => {
 		url = window.location.href;
-		ogImageUrl = `${window.location.origin}/images/og.png`;
+		ogImageUrl = `${window.location.origin}/og.png`;
 	});
 </script>
 
 <svelte:head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="icon" href="/images/favicon.png" />
+	<link rel="icon" href="/favicon.png" />
 	<meta property="og:url" content={url} />
 	<meta property="og:image" content={ogImageUrl} />
 	<meta property="og:type" content="website" />
