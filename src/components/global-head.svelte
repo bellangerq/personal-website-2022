a3386719f4174da10f0df742b77f916f3f84b322
Quentin Bellanger <hello@quentin-bellanger.com>
2022-02-18 22:08:23 +0100

    add meta tags and use property

--- src/components/global-head.svelte
+++ src/components/global-head.svelte
@@ -14,7 +14,10 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="icon" href="/images/favicon.png" />
-	<meta name="og:url" content={url} />
-	<meta name="og:image" content={ogImageUrl} />
-	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="og:url" content={url} />
+	<meta property="og:image" content={ogImageUrl} />
+	<meta property="og:type" content="website" />
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:image" content={ogImageUrl} />
 </svelte:head>
--- src/components/page-head.svelte
+++ src/components/page-head.svelte
@@ -5,7 +5,9 @@
 
 <svelte:head>
 	<title>{title}</title>
-	<meta name="og:title" content={title} />
-	<meta name="description" content={description} />
-	<meta name="og:description" content={description} />
+	<meta property="og:title" content={title} />
+	<meta property="description" content={description} />
+	<meta property="og:description" content={description} />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
 </svelte:head>
