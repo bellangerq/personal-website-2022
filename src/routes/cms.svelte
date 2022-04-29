a9340c807440fd7d3abce50a36da0b22f19a537f
Quentin Bellanger <hello@quentin-bellanger.com>
2022-04-29 19:15:02 +0200

    add hidden field with form name

--- src/routes/cms.svelte
+++ src/routes/cms.svelte
@@ -1,6 +1,8 @@
 <h1>Créer une nouvelle photo</h1>
 
 <form name="photo" method="post" enctype="multipart-form/data" data-netlify="true">
+	<input type="hidden" name="form-name" value="photo" />
+
 	<label for="alt" name="alt">Text alternatif</label>
 	<input style="background: lightgray; color: black;" type="text" id="alt" />
 
