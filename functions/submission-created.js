779b580c7221b1da82519b5ea797affe6166610e
Quentin Bellanger <hello@quentin-bellanger.com>
2022-04-29 22:37:02 +0200

    create md and jpg file from data

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import fetch from 'node-fetch';
 // const body = {
 // 	payload: {
 // 		number: 3,
@@ -28,251 +30,59 @@
 // 		site_url: 'https://quentin-bellanger.com',
 // 		form_name: 'photo'
 // 	},
-// 	site: {
-// 		id: '5c63d756-b237-413f-bd9e-bc91aac19e72',
-// 		site_id: '5c63d756-b237-413f-bd9e-bc91aac19e72',
-// 		plan: 'nf_team_dev',
-// 		ssl_plan: null,
-// 		premium: false,
-// 		claimed: true,
-// 		name: 'quentin-bellanger',
-// 		custom_domain: 'quentin-bellanger.com',
-// 		domain_aliases: [],
-// 		password: null,
-// 		notification_email: null,
-// 		url: 'https://quentin-bellanger.com',
-// 		admin_url: 'https://app.netlify.com/sites/quentin-bellanger',
-// 		deploy_id: '62692d29d141650008f6aae1',
-// 		build_id: '',
-// 		deploy_url: 'http://deploy-preview-4--quentin-bellanger.netlify.app',
-// 		state: 'current',
-// 		screenshot_url:
-// 			'https://d33wubrfki0l68.cloudfront.net/626c361ae862120009f9b2ce/screenshot_2022-04-29-19-02-46-0000.png',
-// 		created_at: '2019-01-28T21:27:11.261Z',
-// 		updated_at: '2022-04-29T19:02:45.887Z',
-// 		user_id: '59c3f8750752d077def0c2f6',
-// 		error_message: null,
-// 		ssl: true,
-// 		ssl_url: 'https://quentin-bellanger.com',
-// 		force_ssl: true,
-// 		ssl_status: null,
-// 		max_domain_aliases: 100,
-// 		build_settings: {
-// 			cmd: 'yarn build',
-// 			dir: 'build',
-// 			branch: 'main',
-// 			base: null,
-// 			env: {
-// 				BASE_URL: 'https://quentin-bellanger.com',
-// 				TWITTER_CONSUMER_KEY: 'r4yYX52raW9yqCuAmNCA32g0i',
-// 				TWITTER_CONSUMER_SECRET: 'obdvzxqWj6mi1eApuinUogfspagpmqzSTqSniXUvpoEVNYk6fc',
-// 				TWITTER_ACCESS_TOKEN_KEY: '400342769-X0zxkWilwFJ6IbBbKsfI2SrIz2DUsBRhqtfo52vq',
-// 				TWITTER_ACCESS_TOKEN_SECRET: '8vsF4StRxpI8lIxa6K0BP1uzxk9VNrByr9cjP0NIM4Uur',
-// 				TWITTER_BEARER_TOKEN:
-// 					'AAAAAAAAAAAAAAAAAAAAAB41awEAAAAASkg38g7Gy6w3AxPOr%2BK3Xz2ZAxU%3DRuhAQ7JkWvvzHWKoDr1aqzMao4N23XpgaatHYBgse025p9OMgf'
-// 			},
-// 			allowed_branches: ['main'],
-// 			skip_prs: false,
-// 			untrusted_flow: 'review',
-// 			private_logs: false,
-// 			functions_dir: 'functions',
-// 			stop_builds: false
-// 		},
-// 		processing_settings: {
-// 			css: { bundle: true, minify: true },
-// 			js: { bundle: true, minify: true },
-// 			images: { optimize: true },
-// 			html: { pretty_urls: true },
-// 			skip: true,
-// 			ignore_html_forms: false
-// 		},
-// 		prerender: null,
-// 		prerender_headers: null,
-// 		deploy_hook: 'https://api.netlify.com/hooks/github',
-// 		published_deploy: {
-// 			id: '62692d29d141650008f6aae1',
-// 			site_id: '5c63d756-b237-413f-bd9e-bc91aac19e72',
-// 			build_id: '62692d29d141650008f6aadf',
-// 			state: 'ready',
-// 			name: 'quentin-bellanger',
-// 			url: 'https://quentin-bellanger.com',
-// 			ssl_url: 'https://quentin-bellanger.com',
-// 			admin_url: 'https://app.netlify.com/sites/quentin-bellanger',
-// 			deploy_url: 'http://main--quentin-bellanger.netlify.app',
-// 			deploy_ssl_url: 'https://main--quentin-bellanger.netlify.app',
-// 			created_at: '2022-04-27T11:46:49.481Z',
-// 			updated_at: '2022-04-29T19:02:45.828Z',
-// 			user_id: '59c3f8750752d077def0c2f6',
-// 			error_message: null,
-// 			required: [],
-// 			required_functions: [],
-// 			commit_ref: 'd93dc46b80eeb88f9e95c7f0e97bb59d9566f522',
-// 			review_id: null,
-// 			branch: 'main',
-// 			commit_url:
-// 				'https://github.com/bellangerq/personal-website-2022/commit/d93dc46b80eeb88f9e95c7f0e97bb59d9566f522',
-// 			skipped: null,
-// 			locked: null,
-// 			log_access_attributes: {
-// 				type: 'firebase',
-// 				url: 'https://netlify-builds5.firebaseio.com/builds/62692d29d141650008f6aadf/log',
-// 				endpoint: 'https://netlify-builds5.firebaseio.com',
-// 				path: '/builds/62692d29d141650008f6aadf/log',
-// 				token:
-// 					'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJpYXQiOjE2NTEyNTkwNjAsImQiOnsidWlkIjoiIn19.FxNoDT3fGS8Zo4eDCTFFsAmrY6WuQB07WrG3wwhiDDk'
-// 			},
-// 			title: 'Delete “static/photos/removal.ai-_tmp-62665971c1bee.png”',
-// 			review_url: null,
-// 			published_at: '2022-04-27T11:47:42.377Z',
-// 			context: 'production',
-// 			deploy_time: 51,
-// 			available_functions: [
-// 				{
-// 					n: 'deploy-succeeded',
-// 					d: '78d0b6a46eda2e75f607b79931c828ca35c6f69d7e1c765de60a0b147216e50c',
-// 					id: 'edc9e575d92ca7d51ff607729e162fa101fcd923c0782266d586f736aaa90fc8',
-// 					s: 166620,
-// 					r: 'nodejs14.x',
-// 					c: '2022-04-14T10:35:25.483Z',
-// 					a: '250705260819'
-// 				},
-// 				{
-// 					n: 'render',
-// 					d: 'b38a83cf7ea638b38233c50ee807447385b5625f84abdf06c153fabfde925b9c',
-// 					id: '8c5cdeeae2e49587196ed72ad54371ca659409343046739cb697cdb7e1f8c575',
-// 					a: '105186839156',
-// 					c: '2022-04-27T11:47:38.145Z',
-// 					r: 'nodejs14.x',
-// 					s: 358879
-// 				}
-// 			],
-// 			screenshot_url:
-// 				'https://d33wubrfki0l68.cloudfront.net/62692d29d141650008f6aae1/screenshot_2022-04-27-11-47-42-0000.png',
-// 			site_capabilities: {
-// 				title: 'Netlify Team Free',
-// 				asset_acceleration: true,
-// 				form_processing: true,
-// 				cdn_propagation: 'partial',
-// 				domain_aliases: true,
-// 				secure_site: false,
-// 				prerendering: true,
-// 				proxying: true,
-// 				ssl: 'custom',
-// 				rate_cents: 0,
-// 				yearly_rate_cents: 0,
-// 				ipv6_domain: 'cdn.makerloop.com',
-// 				branch_deploy: true,
-// 				managed_dns: true,
-// 				geo_ip: true,
-// 				split_testing: true,
-// 				id: 'nf_team_dev',
-// 				cdn_tier: 'reg',
-// 				functions: {
-// 					invocations: { included: 125000, unit: 'requests', used: 6874 },
-// 					runtime: { included: 360000, unit: 'seconds', used: 2171 }
-// 				},
-// 				identity: {
-// 					active_users: { included: 1000, unit: 'users', used: 0 },
-// 					invite_only_users: { included: 5, unit: 'users', used: 1 }
-// 				},
-// 				forms: {
-// 					submissions: { included: 100, unit: 'submissions', used: 0 },
-// 					storage: { included: 10485760, unit: 'bytes', used: 0 },
-// 					use_functions: true
-// 				}
-// 			},
-// 			committer: 'bellangerq',
-// 			skipped_log: null,
-// 			manual_deploy: false,
-// 			file_tracking_optimization: true,
-// 			plugin_state: 'none',
-// 			lighthouse_plugin_scores: null,
-// 			links: {
-// 				permalink: 'https://62692d29d141650008f6aae1--quentin-bellanger.netlify.app',
-// 				alias: 'https://quentin-bellanger.com',
-// 				branch: null
-// 			},
-// 			framework: 'svelte-kit',
-// 			entry_path: null,
-// 			views_count: null,
-// 			function_schedules: [],
-// 			public_repo: true,
-// 			pending_review_reason: null
-// 		},
-// 		managed_dns: true,
-// 		jwt_secret: null,
-// 		jwt_roles_path: 'app_metadata.roles',
-// 		account_slug: 'bellangerq',
-// 		account_name: "Quentin Bellanger's team",
-// 		account_type: 'Starter',
-// 		capabilities: {
-// 			title: 'Netlify Team Free',
-// 			asset_acceleration: true,
-// 			form_processing: true,
-// 			cdn_propagation: 'partial',
-// 			domain_aliases: true,
-// 			secure_site: false,
-// 			prerendering: true,
-// 			proxying: true,
-// 			ssl: 'custom',
-// 			rate_cents: 0,
-// 			yearly_rate_cents: 0,
-// 			ipv6_domain: 'cdn.makerloop.com',
-// 			branch_deploy: true,
-// 			managed_dns: true,
-// 			geo_ip: true,
-// 			split_testing: true,
-// 			id: 'nf_team_dev',
-// 			cdn_tier: 'reg',
-// 			functions: {
-// 				invocations: { included: 125000, unit: 'requests', used: 6874 },
-// 				runtime: { included: 360000, unit: 'seconds', used: 2171 }
-// 			},
-// 			identity: {
-// 				active_users: { included: 1000, unit: 'users', used: 0 },
-// 				invite_only_users: { included: 5, unit: 'users', used: 1 }
-// 			},
-// 			forms: {
-// 				submissions: { included: 100, unit: 'submissions', used: 0 },
-// 				storage: { included: 10485760, unit: 'bytes', used: 0 },
-// 				use_functions: true
-// 			}
-// 		},
-// 		dns_zone_id: '5c51d09b36b26c017cea83cf',
-// 		identity_instance_id: '626928c448974c14f81305f1',
-// 		use_functions: true,
-// 		use_edge_handlers: null,
-// 		parent_user_id: null,
-// 		automatic_tls_provisioning: null,
-// 		disabled: null,
-// 		lifecycle_state: 'active',
-// 		id_domain: '5c63d756-b237-413f-bd9e-bc91aac19e72.netlify.app',
-// 		use_lm: null,
-// 		build_image: 'focal',
-// 		automatic_tls_provisioning_expired: false,
-// 		analytics_instance_id: null,
-// 		functions_region: null,
-// 		functions_config: { site_created_at: '2019-01-28T21:27:11.261Z' },
-// 		plugins: [],
-// 		account_subdomain: null,
-// 		functions_env: {},
-// 		cdp_enabled: true,
-// 		authlify_token_id: '309e1019-9ef7-4daf-b42c-774f0e268e33',
-// 		use_scheduled_functions: false,
-// 		build_timelimit: null,
-// 		default_domain: 'quentin-bellanger.netlify.app'
-// 	}
-// };
+
+// {
+//   filename: '[removal.ai]_tmp-62665971c1bee.png',
+//   type: 'file',
+//   size: 58350,
+//   url: 'https://d33wubrfki0l68.cloudfront.net/69780775-db5b-4ed9-b887-73492899468a/%5Bremoval.ai%5D_tmp-62665971c1bee.png'
+// }
 
 exports.handler = async (event, context) => {
 	console.log(event);
+	console.log('---------');
 
+	// Extract form data
 	const data = JSON.parse(event.body);
-	const alt = data.payload.data.alt;
-	const image = data.payload.data.image;
+	const { lang, description, alt, syndicate } = data.payload.data;
 
-	console.log('alt: ', alt);
-	console.log('image: ', image);
+	const date = new Date(data.created_at);
+	// const date s= new Date('2022-04-29T19:04:20.113Z');
+	const formattedDate = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${(
+		'0' + date.getDate()
+	).slice(-2)}`;
+	// Pog: https://stackoverflow.com/a/221297
+	const timestamp = Math.floor(+date / 1000);
+
+	// Create Markdown file
+	const slug = `${formattedDate}-${timestamp}`;
+	const markdown = `---\nlang: ${lang}\ndate: ${date}\nsyndicate: ${alt}\nsyndicate: ${syndicate}\n---\n\n${description}`;
+	// const markdown = `---\nlang: ${'fr'}\ndate: ${formattedDate}\nalt: ${'pouet'}\nsyndicate: ${true}\n---\n\n${'frheh, rnrnreoneor eroeigoe'}`;
+
+	fs.writeFile(`src/content/photos/${slug}.md`, markdown, (err) => {
+		if (err) {
+			return {
+				statusCode: 400,
+				body: `❌ Error while creating Markdown file: "${error}"`
+			};
+		}
+		console.log('Markdown file successfully created.');
+	});
+
+	try {
+		const file = fs.createWriteStream(`static/photos/${slug}.jpg`);
+		const url = data.payload.data.image.url;
+		// const url =
+		// 	'https://d33wubrfki0l68.cloudfront.net/69780775-db5b-4ed9-b887-73492899468a/%5Bremoval.ai%5D_tmp-62665971c1bee.png';
+		const response = await fetch(url);
+		response.body.pipe(file);
+		console.log('Image file successfully created.');
+	} catch (error) {
+		return {
+			statusCode: 400,
+			body: `❌ Error while creating image file: "${error}"`
+		};
+	}
 
 	return {
 		statusCode: 200,
--- src/routes/cms.svelte
+++ src/routes/cms.svelte
@@ -7,11 +7,26 @@
 <form name="photo" method="post" enctype="multipart/form-data" netlify>
 	<input type="hidden" name="form-name" value="photo" />
 
-	<label for="alt">Text alternatif</label>
-	<input style="background: lightgray; color: black;" type="text" id="alt" name="alt" />
+	<fieldset>
+		<legend>Langue</legend>
+		<label for="lang-fr">Français</label>
+		<input type="radio" id="lang-fr" name="lang" value="fr" checked />
+
+		<label for="lang-en">Anglais</label>
+		<input type="radio" id="lang-en" name="lang" value="en" />
+	</fieldset>
+
+	<label for="description">Description</label>
+	<textarea id="description" name="description" />
 
 	<label for="image">Image</label>
 	<input type="file" id="image" name="image" />
+
+	<label for="alt">Text alternatif</label>
+	<input type="text" id="alt" name="alt" />
+
+	<label for="syndicate">Syndiquer</label>
+	<input type="checkbox" id="syndicate" name="syndicate" checked />
 
 	<button type="submit">Envoyer</button>
 </form>
