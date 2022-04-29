5765407e0777a4311ad358594aca52675840a1da
Quentin Bellanger <hello@quentin-bellanger.com>
2022-04-29 18:49:11 +0200

    add basic netlify form + function

---
+++ functions/submission-created.js
@@ -0,0 +1,11 @@
+exports.handler = async (event, context) => {
+	console.log(event);
+	console.log(context);
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			pouet: 'pouet'
+		})
+	};
+};
---
+++ src/routes/cms.svelte
@@ -0,0 +1,11 @@
+<h1>Créer une nouvelle photo</h1>
+
+<form name="photo" enctype="multipart-form/data" netlify>
+	<label for="alt" name="alt">Text alternatif</label>
+	<input style="background: lightgray; color: black;" type="text" id="alt" />
+
+	<label for="image">Image</label>
+	<input type="file" id="image" />
+
+	<button type="submit">Envoyer</button>
+</form>
