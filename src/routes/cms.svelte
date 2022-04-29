4a53b434ffbcb1210fcc583b9fd6d6fb86e0e9cf
Quentin Bellanger <hello@quentin-bellanger.com>
2022-04-29 21:14:42 +0200

    parse body response

--- src/routes/cms.svelte
+++ src/routes/cms.svelte
@@ -4,10 +4,10 @@
 
 <h1>Créer une nouvelle photo</h1>
 
-<form name="photo" method="post" enctype="multipart-form/data" netlify>
+<form name="photo" method="post" enctype="multipart/form-data" netlify>
 	<input type="hidden" name="form-name" value="photo" />
 
-	<label for="alt" name="alt">Text alternatif</label>
+	<label for="alt">Text alternatif</label>
 	<input style="background: lightgray; color: black;" type="text" id="alt" name="alt" />
 
 	<label for="image">Image</label>
