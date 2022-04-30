c1fcdc586e9cb91baf17cf5ceefb9ad1f8f103b9
Quentin Bellanger <hello@quentin-bellanger.com>
2022-04-30 16:41:39 +0200

    Setup netlify CMS for photos (#4)
    * setup netlify cms
    
    * add basic netlify form + function
    
    * add form method
    
    * use data-netlify attr
    
    * add hidden field with form name
    
    * prerender cms page
    
    * add name attributes to fields
    
    * add netlify cms photos collection config
    
    * write cms flow to readme
    
    * parse body response
    
    * create md and jpg file from data
    
    * fix
    
    * handle dev env
    
    * use buffer to create image
    
    * New photo: 'pouet'
    
    * Changes via API
    
    * do nothing if not on main branch
    
    * add packages
    
    * Add new photo: bla-bla-bla
    
    * Add new photo: 2022-04-29-1651259060
    
    * Delete 2022-04-29-1651259060.md
    
    * Add new photo: 2022-04-29-1651259060
    
    * Add new photo: 2022-04-29-1651259060
    
    * Delete 2022-04-29-1651259060.jpg
    
    * Delete 2022-04-29-1651259060.md
    
    * Add new photo: 2022-04-29-1651259060
    
    * Add new photo: 2022-04-29-1651259060
    
    * Delete 2022-04-29-1651259060.jpg
    
    * Delete 2022-04-29-1651259060.md
    
    * Add new photo: 2022-04-29-1651259060
    
    * Delete 2022-04-29-1651259060.md
    
    * first online test
    
    * delete test image
    
    * Add new photo: 2022-04-29-1651259060
    
    * clean function code
    
    * remove test files
    
    * add some logs
    
    * Add new photo: 2022-04-29-1651259060
    
    * use data from form
    
    * remove test files
    
    * Add new photo: 2022-04-30-1651324179
    
    * fix data from payload
    
    * Add new photo: 2022-04-30-1651325628
    
    * protect with passwordd
    
    * Add new photo: 2022-04-30-1651327422
    
    * unindex cms page and switch to main branch
    
    * delete test files

---
+++ src/routes/cms.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<h1>Créer une nouvelle photo</h1>
+
+<form name="photo" method="post" enctype="multipart/form-data" netlify>
+	<input type="hidden" name="form-name" value="photo" />
+
+	<fieldset>
+		<legend>Langue</legend>
+		<label for="lang-fr">Français</label>
+		<input type="radio" id="lang-fr" name="lang" value="fr" checked />
+
+		<label for="lang-en">Anglais</label>
+		<input type="radio" id="lang-en" name="lang" value="en" />
+	</fieldset>
+
+	<label for="description">Description</label>
+	<textarea id="description" name="description" />
+
+	<label for="image">Image</label>
+	<input type="file" id="image" name="image" />
+
+	<label for="alt">Text alternatif</label>
+	<input type="text" id="alt" name="alt" />
+
+	<label for="syndicate">Syndiquer</label>
+	<input type="checkbox" id="syndicate" name="syndicate" checked />
+
+	<label for="password">Mot de passe</label>
+	<input type="password" id="password" name="password" />
+
+	<button type="submit">Envoyer</button>
+</form>
