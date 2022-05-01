ce732839aaf10ca9c08ae564400e38a8fd81e27e
Quentin Bellanger <hello@quentin-bellanger.com>
2022-05-01 12:08:26 +0200

    style photo form

--- src/routes/cms.svelte
+++ src/routes/cms.svelte
@@ -2,34 +2,93 @@
 	export const prerender = true;
 </script>
 
-<h1>Créer une nouvelle photo</h1>
+<script>
+	import PageHead from '../components/page-head.svelte';
 
-<form name="photo" method="post" enctype="multipart/form-data" netlify>
+	const maxLength = 230;
+	let description = '';
+	$: remainingChar = maxLength - description.length;
+</script>
+
+<PageHead
+	title="Publier une nouvelle photo - Quentin Bellanger"
+	description="Publier une nouvelle photo qui sera disponible sur la page des photos. Et peut-être sur les réseaux."
+/>
+
+<h1>Publier une nouvelle photo</h1>
+
+<form
+	class="form"
+	name="photo"
+	method="post"
+	enctype="multipart/form-data"
+	netlify
+	netlify-honeypot="bot-field"
+>
 	<input type="hidden" name="form-name" value="photo" />
+	<input class="spam-field visually-hidden" name="bot-field" />
+
+	<div class="field">
+		<label for="image">
+			Image
+			<small class="hint">Idéalement une image carrée ou au format paysage.</small>
+		</label>
+		<input class="image-field" required accept="image/*" type="file" id="image" name="image" />
+	</div>
 
 	<fieldset>
 		<legend>Langue</legend>
-		<label for="lang-fr">Français</label>
-		<input type="radio" id="lang-fr" name="lang" value="fr" checked />
+		<small class="hint">Idéalement une image carrée ou au format paysage.</small>
+		<div class="subfield">
+			<input type="radio" id="lang-fr" name="lang" value="fr" checked />
+			<label for="lang-fr"> Français </label>
+		</div>
 
-		<label for="lang-en">Anglais</label>
-		<input type="radio" id="lang-en" name="lang" value="en" />
+		<div class="subfield">
+			<input type="radio" id="lang-en" name="lang" value="en" />
+			<label for="lang-en">Anglais</label>
+		</div>
 	</fieldset>
 
-	<label for="description">Description</label>
-	<textarea id="description" name="description" />
+	<div class="field">
+		<label for="description">
+			Description
+			<small class="hint">{remainingChar} caractères restant.</small>
+		</label>
+		<textarea bind:value={description} required id="description" name="description" rows="5" />
+	</div>
 
-	<label for="image">Image</label>
-	<input type="file" id="image" name="image" />
+	<div class="field">
+		<label for="alt">Text alternatif</label>
+		<textarea required id="alt" name="alt" rows="5" />
+	</div>
 
-	<label for="alt">Text alternatif</label>
-	<input type="text" id="alt" name="alt" />
+	<div class="subfield">
+		<input type="checkbox" id="syndicate" name="syndicate" checked />
+		<label for="syndicate">Syndiquer</label>
+	</div>
 
-	<label for="syndicate">Syndiquer</label>
-	<input type="checkbox" id="syndicate" name="syndicate" checked />
+	<div class="field">
+		<label for="password">Mot de passe</label>
+		<input class="password-field" required type="password" id="password" name="password" />
+	</div>
 
-	<label for="password">Mot de passe</label>
-	<input type="password" id="password" name="password" />
-
-	<button type="submit">Envoyer</button>
+	<button class="submit" type="submit">Envoyer</button>
 </form>
+
+<style lang="scss">
+	.form {
+		display: grid;
+		gap: 1rem;
+		max-width: toRem(600);
+	}
+
+	.image-field,
+	.password-field {
+		width: min(100%, toRem(300));
+	}
+
+	.spam-field {
+		visibility: hidden;
+	}
+</style>
